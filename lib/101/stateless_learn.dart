9c913fe75d19fc30a60deb2ee949bb1d9b1b9e02
Deha Efecan Süer <dehasuer97@gmail.com>
2023-03-21 19:33:39 +0300

    .

--- lib/101/stateless_learn.dart
+++ lib/101/stateless_learn.dart
@@ -4,7 +4,27 @@ class StatelessLearn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          TitleTextWidget(),
+          TitleTextWidget(),
+          TitleTextWidget(),
+          TitleTextWidget(),
+        ],
+      ),
+    );
+  }
+}
+
+class TitleTextWidget extends StatelessWidget {
+  const TitleTextWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "data",
+      style: Theme.of(context).textTheme.headline3,
     );
   }
 }
